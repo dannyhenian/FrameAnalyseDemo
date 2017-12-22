179697862ab69965e56c03e63b9dfdac0f0ce3cf
Danny <3138001703@qq.com>
2017-12-22 15:36:33 +0800

    新增二维码生成解析工具

---
+++ GuavaAnalyse/src/main/java/com/danny/apiTest/collect/BiMapTest.java
@@ -0,0 +1,13 @@
+package com.danny.apiTest.collect;
+/**
+ * Created by danny on 2017-12-22.
+ */
+
+/**
+ * 个扩展来映射接口，支持反向操作。
+ * @author danny
+ * @create 2017-12-22 15:23
+ */
+
+public class BiMapTest {
+}
